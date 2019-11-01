e90117cddd82bb281e429ff8270b2bcdcc36930c
Tommy Johnson <tommy@kands.org.uk>
2019-11-01 22:12:43 +0000

    pics and timer
    pics and timer added in

---
+++ index.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel = "stylesheet" type = "text/css" href = "stylesheet.css" />
+    <link rel="shortcut icon" type="image/png" href="img/favicon.png"/>
+  </head>
+  <body>
+    <h1>Hourly Reminder to drink water!</h1>
+
+    <script type="text/javascript">
+      // the date being counted down to
+      var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+
+
+    </script>
+
+  </body>
+</html>
